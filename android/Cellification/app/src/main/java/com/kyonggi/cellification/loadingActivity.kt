07d299f4751b061d0c8761cc9a9a841e822fe11c
김민종 <alswhddkdl25@naver.com>
2022-04-09 01:11:04 +0900

    FEAT: splash screen 구성 및 splash->main 화면 전환 구현

---
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/loadingActivity.kt
@@ -0,0 +1,26 @@
+package com.kyonggi.cellification
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class loadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+        goToMain()
+    }
+
+    private fun goToMain() {
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+
+    }
+}
