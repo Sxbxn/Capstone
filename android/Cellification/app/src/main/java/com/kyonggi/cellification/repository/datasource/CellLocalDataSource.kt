06183f012afd68a9b6a746fec04d6f951fee472b
Joonhoo <mnc5@naver.com>
2022-04-12 21:14:11 +0900

    FEAT: Cell model 관련 Local 및 Remote 기능 추가

---
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/repository/datasource/CellLocalDataSource.kt
@@ -0,0 +1,6 @@
+package com.kyonggi.cellification.repository.datasource
+
+import com.kyonggi.cellification.data.model.cell.Cell
+
+interface CellLocalDataSource {
+}
---
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/repository/datasourceImpl/CellRemoteDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.kyonggi.cellification.repository.datasourceImpl
+
+import com.kyonggi.cellification.data.model.cell.Cell
+import com.kyonggi.cellification.data.remote.api.CellServiceRequestFactory
+import com.kyonggi.cellification.repository.datasource.CellRemoteDataSource
+import retrofit2.Response
+
+class CellRemoteDataSourceImpl: CellRemoteDataSource {
+
+    override suspend fun getAllCells(): Response<List<Cell>> {
+       return CellServiceRequestFactory.retrofit.getAllCells()
+    }
+
+    override suspend fun getCell(cellid: Int): Response<Cell> {
+        return CellServiceRequestFactory.retrofit.getCell(cellid)
+    }
+
+    override suspend fun deleteCell(cellid: Int): Response<Unit> {
+        return CellServiceRequestFactory.retrofit.deleteCell(cellid)
+    }
+}
