e40d0c79dadebc7a2f09aae4eb932a3bcea115f1
KimTaeKang57 <zizoen123@gmail.com>
2022-04-13 12:26:40 +0900

    회원 탈퇴시 userId에 해당하는 cell 모두삭제, cellId에 해당하는 cell 조회, 삭제 구현

--- lab/capstone-cell-service/src/main/java/com/example/capstonecellservice/jpa/CellRepository.java
+++ lab/capstone-cell-service/src/main/java/com/example/capstonecellservice/jpa/CellRepository.java
@@ -1,12 +1,18 @@
 package com.example.capstonecellservice.jpa;
 
-import com.example.capstonecellservice.dto.CellDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CellRepository extends CrudRepository<CellEntity, Long> {
     Iterable<CellEntity> findCellsByUserId(String userId);
 
     CellEntity findByCellId(String cellId);
+
+    void deleteByUserId(String userId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from CellEntity c where c.userId = :userId and c.cellId = :cellId")
+    void deleteOneCell(String userId, String cellId);
 }
