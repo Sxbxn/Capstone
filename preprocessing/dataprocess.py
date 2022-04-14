295b99cad7f53f0e807af70c922a5c936824c9bd
donhyun han <mpolio2@kyonggi.ac.kr>
2022-04-14 18:20:16 +0900

    FEAT: add YOLOX model

--- preprocessing/dataprocess.py
+++ preprocessing/dataprocess.py
@@ -9,15 +9,15 @@ patch_x_size = 408
 patch_y_size = 306
 x_stride = 204
 y_stride = 153
-input_dir_image = 'data\\result_detection\\'
-output_dir = 'preprocessing\\label\\'
+input_dir_image = '/mnt/d/Git/Capstone/data/result_detection/'
+output_dir = '/mnt/d/Git/Capstone/preprocessing/label/'
 
 img_list = glob.glob(input_dir_image+'*.csv')
 
 for imname in img_list:
     df = pd.read_csv(imname)
     df = df.iloc[:, :-1]
-    imname=imname.split("\\")[-1]
+    imname=imname.split('/')[-1]
 
     for i in range(0,(img_x_size-patch_x_size)//x_stride+1,1):
         for k in range(0,(img_y_size-patch_y_size)//y_stride+1,1):
