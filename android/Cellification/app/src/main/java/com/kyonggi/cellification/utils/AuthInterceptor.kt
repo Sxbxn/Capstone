c452612c399803c0c6907f63901739c195138dfc
Joonhoo <mnc5@naver.com>
2022-04-14 20:32:32 +0900

    FEAT: App에 shared preferences 및 interceptor 추가

---
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/utils/AuthInterceptor.kt
@@ -0,0 +1,14 @@
+package com.kyonggi.cellification.utils
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request =
+            chain.request().newBuilder()
+                .addHeader("Authorization", App.prefs.token ?: "")
+                .build()
+        return chain.proceed(request)
+    }
+}
