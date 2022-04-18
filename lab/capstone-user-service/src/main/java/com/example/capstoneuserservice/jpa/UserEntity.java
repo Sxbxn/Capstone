8961cd227e44b04eee3542afd03aaf79c20e2857
Taekang Kim <zizoen123@gmail.com>
2022-04-18 18:34:21 +0900

    파일 병합

---
+++ lab/capstone-user-service/src/main/java/com/example/capstoneuserservice/jpa/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.capstoneuserservice.jpa;
+
+import com.example.capstoneuserservice.dto.UserDto;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String encryptedPwd;
+
+}
