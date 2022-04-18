3016a60b4a5f90058f3ab5c66e283d41b2654967
Taekang Kim <zizoen123@gmail.com>
2022-04-18 18:47:17 +0900

    merge

---
+++ lab/capstone-apigateway-service/src/main/java/com/example/capstoneapigatewayservice/CapstoneApigatewayServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.capstoneapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CapstoneApigatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApigatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
+}
