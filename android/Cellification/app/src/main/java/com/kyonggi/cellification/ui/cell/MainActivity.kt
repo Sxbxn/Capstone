55117a756a5cb3ae7f7134f63104c8455b029666
Joonhoo <mnc5@naver.com>
2022-04-21 13:15:15 +0900

    REFACTOR: user 회원가입 및 로그인 추가 및 바텀 네비게이션 추가

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/cell/MainActivity.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/cell/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kyonggi.cellification.ui.cell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.kyonggi.cellification.R
 import com.kyonggi.cellification.databinding.ActivityMainBinding
 
@@ -11,5 +12,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initBottomNav()
+    }
+
+    private fun initBottomNav() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> {
+                        changeFragment(AnalysisFragment())
+                    }
+                    R.id.storage -> {
+                        changeFragment(StorageFragment())
+                    }
+                    R.id.settting -> {
+                        changeFragment(SettingsFragment())
+                    }
+                }
+            true
+        }
+        binding.bottomNavigationView.selectedItemId = R.id.home
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
     }
 }
