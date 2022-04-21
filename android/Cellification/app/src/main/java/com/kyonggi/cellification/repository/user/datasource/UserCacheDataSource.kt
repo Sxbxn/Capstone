7a9890ae6a4871e076b68f34fd9378df3d67c2a4
Joonhoo <mnc5@naver.com>
2022-04-21 13:11:48 +0900

    CHORE: User 필요없는 클래스 삭제

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/repository/user/datasource/UserCacheDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.kyonggi.cellification.repository.user.datasource
-
-import com.kyonggi.cellification.data.model.user.User
-
-interface UserCacheDataSource {
-    suspend fun getUsersFromCache(): List<User>
-    suspend fun saveUsersToCache(users: List<User>)
-}
