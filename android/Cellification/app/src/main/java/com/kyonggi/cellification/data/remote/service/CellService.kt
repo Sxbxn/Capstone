fdab7d1462bdf4ff93dc65ba02c6af35f172d6c1
minjong kim <alswhddkdl25@naver.com>
2022-04-22 21:24:32 +0900

    FEAT: cell service interface 1차 구현

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/data/remote/service/CellService.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/data/remote/service/CellService.kt
@@ -1,6 +1,7 @@
 package com.kyonggi.cellification.data.remote.service
 
 import com.kyonggi.cellification.data.model.cell.Cell
+import com.kyonggi.cellification.data.model.cell.ResponseCell
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -9,16 +10,35 @@ interface CellService {
 //    @GET("/capstone-cell-service/cells/")
 //    suspend fun getAllCells(): Response<List<Cell>>
 
+    //cell 생성 testing
+    @POST("/capstone-cell-service/{userId}/cells")
+    suspend fun makeCell(
+        @Path("userId") userId: String
+    ):Response<ResponseCell>
+
     // 특정 user의 Cell List 리턴
     @GET("/capstone-cell-service/{userId}/cells")
     suspend fun getCellListFromUser(
         @Path("userId") userId: String
-    ): Response<List<Cell>>
+    ): Response<ResponseCell>
 
+    // cellid를 통한 특정 cell 조회
+    @GET("/capstone-cell-service/cells/{cellId}")
+    suspend fun getCellInfoFromCellID(
+        @Path("cellId") cellId: String
+    )
+    @DELETE("/capstone-cell-service/{userId}")
+    suspend fun deleteSpecificCell(
+        @Path("userId") userId: String
+    )
     // 특정 분석한 Cell 정보 삭제
     @DELETE("/capstone-cell-service/{userId}/{cellId}")
     suspend fun deleteCell(
         @Path("userId") userId: String,
         @Path("cellId") cellId: String
-    ): Response<Unit>
+    ): Response<Void>
+
+    // cell 이미지 보내기(분석)
+
+
 }
