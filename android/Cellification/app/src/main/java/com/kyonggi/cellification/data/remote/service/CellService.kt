087c16866541fdb47ea667a531d8a39edb4a20c4
minjong kim <alswhddkdl25@naver.com>
2022-04-24 01:47:26 +0900

    FEAT: cell service 구현 및 RemoteDataSource에 연결

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/data/remote/service/CellService.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/data/remote/service/CellService.kt
@@ -1,7 +1,8 @@
 package com.kyonggi.cellification.data.remote.service
 
-import com.kyonggi.cellification.data.model.cell.Cell
 import com.kyonggi.cellification.data.model.cell.ResponseCell
+import com.kyonggi.cellification.data.model.cell.ResponseSpecificUserCell
+import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -20,25 +21,32 @@ interface CellService {
     @GET("/capstone-cell-service/{userId}/cells")
     suspend fun getCellListFromUser(
         @Path("userId") userId: String
-    ): Response<ResponseCell>
+    ): Response<ResponseSpecificUserCell>
 
     // cellid를 통한 특정 cell 조회
     @GET("/capstone-cell-service/cells/{cellId}")
     suspend fun getCellInfoFromCellID(
         @Path("cellId") cellId: String
-    )
+    ): Response<ResponseCell>
+
+    //특정 user의 전체 cell 삭제
     @DELETE("/capstone-cell-service/{userId}")
-    suspend fun deleteSpecificCell(
+    suspend fun deleteAllCell(
         @Path("userId") userId: String
-    )
-    // 특정 분석한 Cell 정보 삭제
+    ): Response<Void>
+
+    // 특정 user의 특정 분석한 Cell 정보 삭제
     @DELETE("/capstone-cell-service/{userId}/{cellId}")
-    suspend fun deleteCell(
+    suspend fun deleteSpecificCell(
         @Path("userId") userId: String,
         @Path("cellId") cellId: String
     ): Response<Void>
 
     // cell 이미지 보내기(분석)
-
+    @Multipart
+    @POST("")
+    suspend fun sendCellImage(
+        @Part cellImage: MultipartBody.Part?
+    ) :Response<Void>
 
 }
--- android/Cellification/app/src/main/java/com/kyonggi/cellification/repository/cell/datasourceImpl/CellRemoteDataSourceImpl.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/repository/cell/datasourceImpl/CellRemoteDataSourceImpl.kt
@@ -1,21 +1,41 @@
 package com.kyonggi.cellification.repository.cell.datasourceImpl
 
 import com.kyonggi.cellification.data.model.cell.Cell
-import com.kyonggi.cellification.data.remote.api.CellServiceRequestFactory
-import com.kyonggi.cellification.repository.cell.datasource.CellRemoteDataSource
-import retrofit2.Response
+import com.kyonggi.cellification.data.model.cell.ResponseCell
+import com.kyonggi.cellification.data.model.cell.ResponseSpecificUserCell
+import com.kyonggi.cellification.data.remote.service.CellService
 
-//class CellRemoteDataSourceImpl: CellRemoteDataSource {
-//
-//    override suspend fun getAllCells(): Response<List<Cell>> {
-//       return CellServiceRequestFactory.retrofit.getAllCells()
-//    }
-//
-//    override suspend fun getCell(cellid: Int): Response<Cell> {
-//        return CellServiceRequestFactory.retrofit.getCell(cellid)
-//    }
-//
-//    override suspend fun deleteCell(cellid: Int): Response<Unit> {
-//        return CellServiceRequestFactory.retrofit.deleteCell(cellid)
-//    }
-//}
+import com.kyonggi.cellification.repository.cell.datasource.CellRemoteDataSource
+import okhttp3.MultipartBody
+import retrofit2.Response
+import javax.inject.Inject
+
+class CellRemoteDataSourceImpl(
+    private val cellService: CellService
+): CellRemoteDataSource {
+    override suspend fun makeCell(userid: String): Response<ResponseCell> {
+        return cellService.makeCell(userid)
+    }
+
+    override suspend fun getCellListFromUser(userid: String): Response<ResponseSpecificUserCell>{
+        return cellService.getCellListFromUser(userid)
+    }
+
+    override suspend fun getCellInfoFromCellID(cellid: String): Response<ResponseCell> {
+       return cellService.getCellInfoFromCellID(cellid)
+    }
+
+    override suspend fun deleteAllCell(userid: String): Response<Void> {
+        return cellService.deleteAllCell(userid)
+    }
+
+    override suspend fun deleteSpecificCell(userid: String, cellid: String): Response<Void> {
+        return cellService.deleteSpecificCell(userid,cellid)
+    }
+
+    override suspend fun sendCellImage(image: MultipartBody.Part): Response<Void> {
+        return cellService.sendCellImage(image)
+    }
+
+
+}
