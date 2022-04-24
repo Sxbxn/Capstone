753ab35e9139bb78f6eb552886df6b215c8d65f1
minjong kim <alswhddkdl25@naver.com>
2022-04-25 01:24:57 +0900

    CHORE: 변수 조정 및 주석 추가

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/data/model/cell/ResponseCell.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/data/model/cell/ResponseCell.kt
@@ -2,6 +2,7 @@ package com.kyonggi.cellification.data.model.cell
 
 import com.google.gson.annotations.SerializedName
 
+// 로그인 안했을시 사용? 저장은 못하게..
 data class ResponseCell(
     @SerializedName("cellId")
     val cellId: String,
--- android/Cellification/app/src/main/java/com/kyonggi/cellification/data/model/cell/ResponseSpecificUserCell.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/data/model/cell/ResponseSpecificUserCell.kt
@@ -3,19 +3,20 @@ package com.kyonggi.cellification.data.model.cell
 import com.google.gson.annotations.SerializedName
 import java.util.Date
 
+//로그인 했을 시 사용
 data class ResponseSpecificUserCell(
+    @SerializedName("ID")
+    val id: Int,
     @SerializedName("cellId")
     val cellId: String,
-    @SerializedName("totalCell")
-    val totalCell: Int,
-    @SerializedName("liveCell")
-    val liveCell: Int,
-    @SerializedName("deadCell")
-    val deadCell: Int,
-    @SerializedName("viability")
-    val viability: Double,
     @SerializedName("createAt")
     val createAt: Date,
+    @SerializedName("deadCell")
+    val deadCell: Int,
+    @SerializedName("liveCell")
+    val liveCell: Int,
+    @SerializedName("viability")
+    val viability: Double,
     @SerializedName("userid")
     val userId: String
 )
