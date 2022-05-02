b6067408bd72481d0c71dcded1d727de064cc2b7
Joonhoo <mnc5@naver.com>
2022-05-02 20:54:36 +0900

    REFACTOR: 로그인 및 회원가입 코드 수정

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/login/LogInActivity.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/login/LogInActivity.kt
@@ -2,76 +2,106 @@ package com.kyonggi.cellification.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.core.widget.doOnTextChanged
 import com.kyonggi.cellification.data.model.user.UserLogin
 import com.kyonggi.cellification.databinding.ActivityLogInBinding
+import com.kyonggi.cellification.ui.cell.MainActivity
 import com.kyonggi.cellification.ui.di.App
 import com.kyonggi.cellification.ui.viewmodel.UserViewModel
 import com.kyonggi.cellification.utils.*
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LogInActivity : AppCompatActivity() {
     private val userViewModel: UserViewModel by viewModels()
     private lateinit var binding: ActivityLogInBinding
+    private lateinit var loading: LoadingDialog
+    private lateinit var userLogin: UserLogin
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLogInBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        loading = LoadingDialog(this)
+        // 자동 로그인
+        loginCheck()
 
+        // 요청 취소
+        loading.cancelButton().setOnClickListener {
+            loading.setInvisible()
+        }
+        // 요청 다시시도
+        loading.retryButton().setOnClickListener {
+            loading.setInvisible()
+            requestLogin(userLogin)
+        }
+
+        userViewModel.isValidLiveData.observe(this) { isValid ->
+            binding.buttonLogin.isEnabled = isValid
+        }
 
         with(binding) {
             // 회원가입 페이지 이동
             buttonSignin.setOnClickListener {
-                startActivity(Intent(this@LogInActivity, SignInActivity::class.java))
+                startActivity(Intent(this@LogInActivity, SignInActivity::class.java).apply {
+                    this.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                })
             }
-            // 로그인 요청
+
+            editTextLoginId.doOnTextChanged { text, _, _, _ ->
+                initValidate(emailInputLayout)
+                userViewModel.emailLiveData.value = text.toString()
+            }
+            editTextLoginPwd.doOnTextChanged { text, _, _, _ ->
+                userViewModel.pwdLiveData.value = text.toString()
+            }
             buttonLogin.setOnClickListener {
-                if (checkValidation(editTextLoginId, editTextLoginPwd)) {
-                    val userLogin =
-                        UserLogin(editTextLoginId.text.toString(), editTextLoginPwd.text.toString())
+                userLogin =
+                    UserLogin(editTextLoginId.text.toString(), editTextLoginPwd.text.toString())
+
+                if (!validateLoginEmail(emailInputLayout, editTextLoginId.text.toString())) {
+                    shake(editTextLoginId, this@LogInActivity)
+                } else {
+                    // 로그인 요청
                     requestLogin(userLogin)
                 }
             }
         }
     }
 
-    // Validation 체크
-    private fun checkValidation(
-        email: EditText,
-        pwd: EditText,
-    ): Boolean {
-        return validateEmail(email, this) || validateLoginPassword(pwd, this)
-    }
-
     private fun requestLogin(userLogin: UserLogin) {
         userViewModel.getTokenRequest(userLogin)
-        userViewModel.isLogin.observe(this@LogInActivity, Observer { response ->
+        userViewModel.isLogin.observe(this@LogInActivity) { response ->
             when (response) {
                 is APIResponse.Success -> {
                     // success code
-                    Toast.makeText(applicationContext, response.data?.get("token"), Toast.LENGTH_SHORT).show()
-                    Toast.makeText(applicationContext, response.data?.get("userid"), Toast.LENGTH_SHORT).show()
+                    App.prefs.apply {
+                        token = response.data?.get("token")
+                        userId = response.data?.get("userId")
+                    }
+                    startActivity(Intent(this, MainActivity::class.java).apply {
+                        this.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    })
                 }
                 is APIResponse.Error -> {
                     // error code
-                    Toast.makeText(applicationContext, response.message, Toast.LENGTH_SHORT).show()
+                    loading.setError()
                 }
                 is APIResponse.Loading -> {
                     // loading code
-                    Toast.makeText(applicationContext, "로딩중", Toast.LENGTH_SHORT).show()
+                    loading.setVisible()
                 }
             }
-        })
+        }
+    }
+
+    private fun loginCheck() {
+        // JWT Token 로컬에 저장되어있다면 자동 로그인
+        if (!App.prefs.token.isNullOrBlank()) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/login/SignInActivity.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/login/SignInActivity.kt
@@ -1,81 +1,132 @@
 package com.kyonggi.cellification.ui.login
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.core.widget.doOnTextChanged
 import com.kyonggi.cellification.data.model.user.User
 import com.kyonggi.cellification.databinding.ActivitySignInBinding
-import com.kyonggi.cellification.ui.viewmodel.UserViewModel
-import com.kyonggi.cellification.ui.viewmodel.UserViewModelFactory
-import com.kyonggi.cellification.utils.APIResponse
-import com.kyonggi.cellification.utils.validateEmail
-import com.kyonggi.cellification.utils.validateName
-import com.kyonggi.cellification.utils.validateSigninPassword
+import com.kyonggi.cellification.ui.viewmodel.SignInViewModel
+import com.kyonggi.cellification.utils.*
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SignInActivity : AppCompatActivity() {
-    private val userViewModel: UserViewModel by viewModels()
+    private val signInViewModel: SignInViewModel by viewModels()
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var loading: LoadingDialog
+    private lateinit var user: User
+    private lateinit var email: String
+    private lateinit var pwd: String
+    private lateinit var re_pwd: String
+    private lateinit var name: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
+        loading = LoadingDialog(this)
         setContentView(binding.root)
 
+        // 요청 취소
+        loading.cancelButton().setOnClickListener {
+            loading.setInvisible()
+        }
+        // 요청 다시시도
+        loading.retryButton().setOnClickListener {
+            loading.setInvisible()
+            requestSignIn(user)
+        }
+
+        signInViewModel.isValidLiveData.observe(this) { isValid ->
+            binding.buttonSignIn.isEnabled = isValid
+        }
+
         with(binding) {
+            // 로그인 페이지 이동
+            buttonLogIn.setOnClickListener {
+                startActivity(Intent(this@SignInActivity, LogInActivity::class.java).apply {
+                    this.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                })
+            }
+
+            editTextSigninId.doOnTextChanged { text, _, _, _ ->
+                initValidate(emailInputLayout)
+                signInViewModel.emailLiveData.value = text.toString()
+                email = text.toString()
+            }
+
+            editTextSigninPwd.doOnTextChanged { text, _, _, _ ->
+                initValidate(pwdInputLayout)
+                signInViewModel.pwdLiveData.value = text.toString()
+                pwd = text.toString()
+            }
+
+            editTextSigninPwdChk.doOnTextChanged { text, _, _, _ ->
+                initValidate(rePwdInputLayout)
+                signInViewModel.repwdLiveData.value = text.toString()
+                re_pwd = text.toString()
+            }
+
+            editTextSigninName.doOnTextChanged { text, _, _, _ ->
+                initValidate(nameInputLayout)
+                signInViewModel.nameLiveData.value = text.toString()
+                name = text.toString()
+            }
             buttonSignIn.setOnClickListener {
-                if (checkValidation(editTextSigninId, editTextSigninPwd, editTextSigninName)) {
-                    if (editTextSigninPwd.text.toString() != editTextSigninPwdChk.text.toString()) {
-                        Toast.makeText(
-                            this@SignInActivity,
-                            "비밀번호와 비밀번호확인이 일치하지 않습니다..",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        val user = User(
-                            editTextSigninId.text.toString(),
-                            editTextSigninName.text.toString(),
-                            editTextSigninPwd.text.toString()
-                        )
-                        // 회원가입 요청
-                        requestSignIn(user)
-                    }
+                user = User(email, name, pwd)
+                if (checkValidation()) {
+                    // 회원가입 요청
+                    requestSignIn(user)
                 }
             }
         }
-
     }
 
-    // Validation 체크
-    private fun checkValidation(
-        email: EditText,
-        pwd: EditText,
-        name: EditText
-    ): Boolean {
-        return validateEmail(email, this) || validateSigninPassword(pwd, this) || validateName(name, this)
+    private fun checkValidation(): Boolean {
+        var check = true
+        with(binding) {
+            if (!validateEmail(emailInputLayout, email)) {
+                shake(editTextSigninId, this@SignInActivity)
+                check = false
+            }
+            if (!validateSignInPassword(pwdInputLayout, pwd)) {
+                shake(editTextSigninPwd, this@SignInActivity)
+                check = false
+            }
+            if (!validateSignInRePassword(rePwdInputLayout, pwd, re_pwd)) {
+                shake(editTextSigninPwdChk, this@SignInActivity)
+                check = false
+            }
+            if (!validateName(nameInputLayout, name)) {
+                shake(editTextSigninName, this@SignInActivity)
+                check = false
+            }
+            return check
+        }
     }
 
     // 회원가입 요청
     private fun requestSignIn(user: User) {
-        userViewModel.signInUser(user)
-        userViewModel.state.observe(this, Observer { response ->
+        signInViewModel.signInUser(user)
+        signInViewModel.state.observe(this) { response ->
             when (response) {
                 is APIResponse.Success -> {
                     // success code
+                    loading.dismiss()
+                    startActivity(Intent(this, LogInActivity::class.java).apply {
+                        this@SignInActivity.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    })
                 }
                 is APIResponse.Error -> {
                     // error code
+                    loading.setError()
                 }
                 is APIResponse.Loading -> {
                     // loading code
+                    loading.setVisible()
                 }
             }
-        })
+        }
     }
 }
