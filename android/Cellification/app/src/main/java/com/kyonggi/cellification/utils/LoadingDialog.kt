8069825c30b12fda6fe4faa3e9d9c1c74bc4b60c
Joonhoo <mnc5@naver.com>
2022-05-02 20:50:50 +0900

    FEAT: 로딩 dialog 추가

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/utils/LoadingDialog.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/utils/LoadingDialog.kt
@@ -4,16 +4,20 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.view.View
+import android.widget.Button
 import com.kyonggi.cellification.R
+import com.kyonggi.cellification.databinding.DialogLoadingBinding
 
 class LoadingDialog (context: Context) : Dialog(context){
 
+    private var binding: DialogLoadingBinding
     init {
         setCanceledOnTouchOutside(false)
-
+        setCancelable(false)
         window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        setContentView(R.layout.dialog_loading)
+        binding = DialogLoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun setVisible() {
@@ -23,4 +27,16 @@ class LoadingDialog (context: Context) : Dialog(context){
     fun setInvisible() {
         this.dismiss()
     }
+
+    fun setError() {
+        with(binding) {
+            loadingLayout.visibility = View.GONE
+            retryButton.visibility = View.VISIBLE
+            cancelButton.visibility = View.VISIBLE
+        }
+    }
+
+    fun retryButton(): Button = binding.retryButton
+
+    fun cancelButton(): Button = binding.cancelButton
 }
