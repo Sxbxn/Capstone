4250a61266c66dbe6b5afb2ae525c86769891936
Joonhoo <mnc5@naver.com>
2022-05-02 17:42:01 +0900

    REFACTOR: viewmodel 분리 및 코드 수정

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/viewmodel/SignInViewModel.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/viewmodel/SignInViewModel.kt
@@ -6,26 +6,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kyonggi.cellification.data.model.user.ResponseUser
 import com.kyonggi.cellification.data.model.user.User
-import com.kyonggi.cellification.data.model.user.UserLogin
 import com.kyonggi.cellification.repository.user.UserRepository
 import com.kyonggi.cellification.utils.APIResponse
-import com.kyonggi.cellification.utils.validateLoginEmail
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.Headers
 import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel @Inject constructor(
+class SignInViewModel @Inject constructor(
     private val repository: UserRepository
 ): ViewModel() {
 
-    // request state
-    val isLogin: MutableLiveData<APIResponse<Headers>> = MutableLiveData()
-    val withdrawal: MutableLiveData<APIResponse<Void>> = MutableLiveData()
+    val state: MutableLiveData<APIResponse<ResponseUser>> = MutableLiveData()
     val emailLiveData: MutableLiveData<String> = MutableLiveData()
     val pwdLiveData: MutableLiveData<String> = MutableLiveData()
+    val repwdLiveData: MutableLiveData<String> = MutableLiveData()
+    val nameLiveData: MutableLiveData<String> = MutableLiveData()
     val isValidLiveData: MediatorLiveData<Boolean> = MediatorLiveData<Boolean>().apply {
         // 입력이 valid 하지않으면 버튼 disabled
         this.value = false
@@ -37,32 +34,34 @@ class UserViewModel @Inject constructor(
         addSource(pwdLiveData) {
             this.value = validateForm()
         }
+
+        addSource(repwdLiveData) {
+            this.value = validateForm()
+        }
+
+        addSource(nameLiveData) {
+            this.value = validateForm()
+        }
+
     }
     private fun validateForm(): Boolean {
         return !emailLiveData.value.isNullOrBlank() && !pwdLiveData.value.isNullOrBlank()
+                && !repwdLiveData.value.isNullOrBlank() && !nameLiveData.value.isNullOrBlank()
     }
 
-    fun getTokenRequest(userLogin: UserLogin) {
-        isLogin.value = APIResponse.Loading()
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = repository.getAccessToken(userLogin)
+    fun signInUser(user: User) {
+        state.value = APIResponse.Loading()
+        viewModelScope.launch(Dispatchers.IO)  {
+            val response = repository.signInUser(user)
             try {
                 if (response.data != null) {
-                    isLogin.postValue(response)
+                    state.postValue(response)
                 } else {
-                    isLogin.postValue(APIResponse.Error(response.message.toString()))
+                    state.postValue(APIResponse.Error(response.message.toString()))
                 }
             } catch (e: Exception) {
-                isLogin.postValue(APIResponse.Error(e.message.toString()))
+                state.postValue(APIResponse.Error(e.message.toString()))
             }
-        }
-    }
-
-    fun withdrawalUser(token: String, userId: String) {
-        withdrawal.value = APIResponse.Loading()
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = repository.withdrawalUSer(token, userId)
-            withdrawal.postValue(response)
         }
     }
 }
