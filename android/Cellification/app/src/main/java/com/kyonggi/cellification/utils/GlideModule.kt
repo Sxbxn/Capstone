2ed8e26bf60104f9ddc1d2fd8d4787f99790c626
Joonhoo <mnc5@naver.com>
2022-05-03 17:35:22 +0900

    FEAR: GlideModule 추가

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/utils/GlideModule.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/utils/GlideModule.kt
@@ -1,14 +1,7 @@
 package com.kyonggi.cellification.utils
 
-import android.content.Context
-import android.net.Uri
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.kyonggi.cellification.databinding.FragmentAnalysisBinding
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 @GlideModule
 class GlideModule() : AppGlideModule() {
