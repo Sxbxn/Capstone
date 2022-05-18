179fae1042e9b94fca49f04b3aedbf8f452ac073
minjong kim <alswhddkdl25@naver.com>
2022-05-18 13:06:49 +0900

    FEAT: Recycler 각 item에 결과 이미지 추가

--- android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/cell/ResultRecyclerFragment.kt
+++ android/Cellification/app/src/main/java/com/kyonggi/cellification/ui/cell/ResultRecyclerFragment.kt
@@ -1,14 +1,17 @@
 package com.kyonggi.cellification.ui.cell
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kyonggi.cellification.R
-import com.kyonggi.cellification.databinding.FragmentResultBinding
 import com.kyonggi.cellification.databinding.FragmentResultRecyclerBinding
+import com.kyonggi.cellification.utils.GlideApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,6 +36,9 @@ class ResultRecyclerFragment : Fragment() {
 
             initView(totalCell,liveCell,deadCell,viability)
         }
+        binding.showImage.setOnClickListener{
+            showImage()
+        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -44,5 +50,23 @@ class ResultRecyclerFragment : Fragment() {
             liveCell.text = live.toString()
             dieCell.text = dead.toString()
         }
+    }
+    private fun showImage(){
+        val builder = AlertDialog.Builder(requireContext(), R.style.DeleteDialog)
+        val imgView = layoutInflater.inflate(R.layout.result_image,null)
+        GlideApp.with(binding.root)
+            .load("https://capstone-taekang-bucket.s3.ap-northeast-2.amazonaws.com/166443.jpg")
+            .placeholder(R.drawable.ic_baseline_settings_24)
+            .error(R.drawable.ic_baseline_settings_24)
+            .fallback(R.drawable.ic_baseline_settings_24)
+            .override(1000,1000) // 추후조정
+            .into(imgView.findViewById(R.id.result_image))
+
+        val dialog = builder.setTitle("결과 이미지")
+            .setView(imgView)
+            .setPositiveButton("확인"){_,_->}
+            .create()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
     }
 }
